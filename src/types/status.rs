35b7a1b2cba6ac30014812e7c73c6af53c09f564
xingqiang.yuan <xingqiang.yuan@okg.com>
2023-04-20 22:59:13 +0800

    optimize clone and bounding check

--- src/types/status.rs
+++ src/types/status.rs
@@ -22,21 +22,19 @@ fn position_get_status_inner(
     let mut total_risk_rep: BigInt = BigInt::zero();
     let mut value_rep: BigInt = BigInt::zero();
     let mut risk_rep: BigInt = BigInt::zero();
-    for asset in assets.iter() {
-        let curr_asset_id = &asset.asset_id;
-        let balance = asset.balance.clone();
-        for oracle_price in oracle_prices.data.iter() {
-            if oracle_price.asset_id == curr_asset_id.clone() {
+    for asset in assets {
+        for oracle_price in &oracle_prices.data {
+            if &oracle_price.asset_id == &asset.asset_id {
                 // Signed (96.32) fixed point.
-                value_rep = balance.clone() * oracle_price.price.clone();
-                total_value_rep += value_rep.clone();
+                value_rep = &asset.balance * &oracle_price.price;
+                total_value_rep += &value_rep;
             }
         }
-        for synthetic_asset_info in general_config.synthetic_assets_info.iter() {
-            if synthetic_asset_info.asset_id == curr_asset_id.clone() {
-                let abs_value_rep = value_rep.clone().abs();
+        for synthetic_asset_info in &general_config.synthetic_assets_info {
+            if &synthetic_asset_info.asset_id == &asset.asset_id {
+                let abs_value_rep = (&value_rep).abs();
                 // value_rep is a (96.32) fixed point so risk_rep is a (128.64) fixed point.
-                risk_rep = abs_value_rep * synthetic_asset_info.risk_factor.clone();
+                risk_rep = abs_value_rep * &synthetic_asset_info.risk_factor;
                 total_risk_rep += risk_rep;
             }
         }
@@ -62,23 +60,34 @@ pub fn position_get_status(
 
     //let (total_value_rep2, total_risk_rep2) = (total_value_rep.to_i128().unwrap(), total_risk_rep.to_i128().unwrap());
 
-    let total_value_lower_bound_rep = BigInt::from(TOTAL_VALUE_LOWER_BOUND_SHIFT_32);
-    let total_value_upper_bound_rep = BigInt::from(TOTAL_VALUE_UPPER_BOUND_SHIFT_32);
-    if total_value_rep < total_value_lower_bound_rep
-        || total_value_rep >= total_value_upper_bound_rep
-    {
+    if let Some(tv) = total_value_rep.to_i128() {
+        if tv < TOTAL_VALUE_LOWER_BOUND_SHIFT_32
+            || tv >= TOTAL_VALUE_UPPER_BOUND_SHIFT_32 {
+            return Err(PerpError::OutOfRangeTotalValue);
+        }
+    } else {
         return Err(PerpError::OutOfRangeTotalValue);
     }
+    // let total_value_lower_bound_rep = BigInt::from(TOTAL_VALUE_LOWER_BOUND_SHIFT_32);
+    // let total_value_upper_bound_rep = BigInt::from(TOTAL_VALUE_UPPER_BOUND_SHIFT_32);
+    // if total_value_rep < total_value_lower_bound_rep
+    //     || total_value_rep >= total_value_upper_bound_rep
+    // {
+    //     return Err(PerpError::OutOfRangeTotalValue);
+    // }
 
-    //#[allow(arithmetic_overflow)]
-    //let a: i128 = 1<<64;
-    //std::u128::MAX;
-    //let a: u128 = -1;
-    let tr_upper_bound_rep = BigInt::from(TOTAL_RISK_UPPER_BOUND); //* a;
-    //println!("tr_upper_bound_rep: {}", tr_upper_bound_rep);
-    if total_risk_rep >= tr_upper_bound_rep {
+
+    if let Some(tr) = total_risk_rep.to_u128() {
+        if tr >= TOTAL_RISK_UPPER_BOUND {
+            return Err(PerpError::OutOfRangeTotalRisk);
+        }
+    } else {
         return Err(PerpError::OutOfRangeTotalRisk);
     }
+    // let tr_upper_bound_rep = BigInt::from(TOTAL_RISK_UPPER_BOUND);
+    // if total_risk_rep >= tr_upper_bound_rep {
+    //     return Err(PerpError::OutOfRangeTotalRisk);
+    // }
 
     Ok((total_value_rep, total_risk_rep))
 }
